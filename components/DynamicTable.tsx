9dbdfec8b4841fdc23a9eb388471f446d2647edd
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2023-04-26 13:55:15 +0800

    add comments

--- components/DynamicTable.tsx
+++ components/DynamicTable.tsx
@@ -50,20 +50,26 @@ const defaultColumns: ColumnDef<User>[] = [
     width: 200,
   },
 ];
+
+// generate random data
+const data = new Array(10).fill("x").map<User>((item, index) => {
+  return {
+    id: index,
+    email: faker.internet.email(),
+    gender: faker.name.gender(true),
+    ip_address: faker.internet.ipv4(),
+    created_at: faker.date.past().toDateString(),
+    last_visited_at: faker.date.recent().toDateString(),
+  };
+});
+
 const DynamicTable = () => {
-  // generate random data
-  const data = new Array(10).fill("x").map<User>((item, index) => {
-    return {
-      id: index,
-      email: faker.internet.email(),
-      gender: faker.name.gender(true),
-      ip_address: faker.internet.ipv4(),
-      created_at: faker.date.past().toDateString(),
-      last_visited_at: faker.date.recent().toDateString(),
-    };
-  });
   const [columns, setColumns] = useState([...defaultColumns]);
 
+  // logic to pin a specific column
+  // 1. modify the isPinned property
+  // 2. sort the columns so that isPinned is first
+  // 3. use setColumns to re-render the component
   const onPinColumn = (accessorKey: string, isPinned: boolean = false) => {
     const newCols = columns.map((col) => {
       if (col.accessorKey === accessorKey) {
@@ -83,6 +89,9 @@ const DynamicTable = () => {
     return setColumns([...newCols]);
   };
 
+  // logic to get sticky position for columns
+  // 1. find all the previous columns
+  // 2. add the total width of all the previous columns.
   const getLeftStickyPos = (index: number) => {
     if (!index) return 0;
 
@@ -94,69 +103,65 @@ const DynamicTable = () => {
     return prevColumnsTotalWidth;
   };
   return (
-    <div>
-      <div className="overflow-auto max-h-[400px]">
-        <table className="table-fixed w-full">
-          <thead>
-            <tr>
-              {columns.map((col, i) => {
-                return (
-                  <th
-                    className={classNames({
-                      "p-2 text-left whitespace-nowrap": true,
-                      "bg-indigo-500": !col.isPinned,
-                      "sticky bg-indigo-900 text-indigo-50": col.isPinned,
-                    })}
-                    style={{
-                      left: getLeftStickyPos(i),
-                      width: col.width,
-                    }}
-                    key={col.header}
-                  >
-                    {col.header}
-                    <button
-                      onClick={() =>
-                        onPinColumn(col.accessorKey, !col.isPinned)
-                      }
-                      className="mx-2 text-xs text-white"
-                    >
-                      {col.isPinned ? "x" : "pin"}
-                    </button>
-                  </th>
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((item, index) => {
+    <div className="overflow-auto max-h-[400px]">
+      <table className="table-fixed w-full">
+        <thead>
+          <tr>
+            {columns.map((col, i) => {
               return (
-                <tr key={index} className="text-left">
-                  {columns.map((col, i) => {
-                    const accessorKey = col.accessorKey;
-                    const value = item[accessorKey];
-                    return (
-                      <td
-                        className={classNames({
-                          "p-2": true,
-                          "sticky bg-indigo-200": col.isPinned,
-                        })}
-                        style={{
-                          left: getLeftStickyPos(i),
-
-                          width: col.width,
-                        }}
-                        key={col.header}
-                      >
-                        {value}
-                      </td>
-                    );
+                <th
+                  className={classNames({
+                    "p-2 text-left whitespace-nowrap": true,
+                    "bg-indigo-500": !col.isPinned,
+                    "sticky bg-indigo-900 text-indigo-50": col.isPinned,
                   })}
-                </tr>
+                  style={{
+                    left: getLeftStickyPos(i),
+                    width: col.width,
+                  }}
+                  key={col.header}
+                >
+                  {col.header}
+                  <button
+                    onClick={() => onPinColumn(col.accessorKey, !col.isPinned)}
+                    className="mx-2 text-xs text-white"
+                  >
+                    {col.isPinned ? "x" : "pin"}
+                  </button>
+                </th>
               );
             })}
-          </tbody>
-        </table>
-      </div>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => {
+            return (
+              <tr key={index} className="text-left">
+                {columns.map((col, i) => {
+                  const accessorKey = col.accessorKey;
+                  const value = item[accessorKey];
+                  return (
+                    <td
+                      className={classNames({
+                        "p-2": true,
+                        "sticky bg-indigo-200": col.isPinned,
+                      })}
+                      style={{
+                        left: getLeftStickyPos(i),
+
+                        width: col.width,
+                      }}
+                      key={col.header}
+                    >
+                      {value}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
